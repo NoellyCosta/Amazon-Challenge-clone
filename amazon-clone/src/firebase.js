94c7a88dd436ad1f73622afe28e9a5afe60846fd
NoellyCosta <noellysallazar@gmail.com>
2022-07-10 23:57:38 -0300

    Header finished

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABXAlS7sPud8aPj76B876mg1rWEsJIB3M",
+    authDomain: "challenge-7ca39.firebaseapp.com",
+    projectId: "challenge-7ca39",
+    storageBucket: "challenge-7ca39.appspot.com",
+    messagingSenderId: "404596872101",
+    appId: "1:404596872101:web:dc09c135b91d3ec806ce4c",
+    measurementId: "G-V0E7TYDPPF"
+  };
